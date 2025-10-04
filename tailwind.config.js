5ae9e4f406e4797700030474f91c38f8ef6397fa
mauricarron <59117899+mauricarron@users.noreply.github.com>
2025-10-04 12:42:51 -0300

    add util libs for ui components

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,17 @@ module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     // This overrides are to perfectly match the colors in the design. Used https://tailwind-color-finder.vercel.app/ to find the closest match.
-    colors: {
-      green: {
-        500: "#22C55D",
+    extend: {
+      colors: {
+        green: {
+          550: "#22C55D",
+        },
+        red: {
+          550: "#DC2625",
+        },
       },
-      red: {
-        600: "#DC2625",
+      boxShadow: {
+        button: "0px 4px 8px 0px #64748B40",
       },
     },
     fontFamily: {
